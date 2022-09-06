98cd2f9efaef695c1a06fd94d57c3ff9f60771f3
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-06 16:38:10 -0600

    Add basic router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import Home from "./pages/home";
+import Shop from "./pages/shop";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Cart from "./pages/cart";
+import Wishlist from "./pages/wishlist";
+import NoMatch from "./pages/noMatch";
 import "./global.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/shop">Shop</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <span>Miralou</span>
+        <nav>
+          <Link to="/cart">Cart</Link>
+          <Link to="/wishlist">Wishlist</Link>
+        </nav>
+      </header>
+      <Outlet />
+    </>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="wishlist" element={<Wishlist />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
