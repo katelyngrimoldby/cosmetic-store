ab67c2fa909c5d33796805003f0f97eaf3329bdd
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-23 12:14:39 -0600

    Build 404 page

--- src/pages/noMatch.tsx
+++ src/pages/noMatch.tsx
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import astronaut from "../assets/astronaut.jpg";
+import styles from "../styles/noMatch.module.css";
+
 const NoMatch = () => {
   return (
-    <div>
-      <h1>404</h1>
-    </div>
+    <main>
+      <header className={styles.hero}>
+        <div className={styles.content}>
+          <h1>The Page Has Gone Far, Far Away</h1>
+          <p>
+            We can&apos;t find the page you&apos;re looking for. It may have
+            been moved or no longer exists.
+          </p>
+          <span>
+            Go to the <Link to="/">homepage</Link> instead
+          </span>
+        </div>
+        <img src={astronaut} alt="Astronaut" width="788" height="1116" />
+      </header>
+    </main>
   );
 };
 
