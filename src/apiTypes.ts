73a73374a31f9638270a3f45d62b89e6ea2231f6
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-24 17:10:57 -0600

    Write object interface

--- src/apiTypes.ts
+++ src/apiTypes.ts
@@ -1,10 +1,27 @@
-export type obj = {
-  key: string;
-  value: string | string[] | { key: string; value: string };
-};
+export interface colors {
+  hex_value: string;
+  colour_name: string;
+}
+
+export interface obj {
+  api_featured_image: string;
+  brand: string;
+  category: string;
+  created_at: string;
+  currency: string;
+  description: string;
+  id: number;
+  image_link: string;
+  name: string;
+  price: string;
+  price_sign: string;
+  product_api_url: string;
+  product_colors: colors[];
+  product_link: string;
+  product_type: string;
+  tag_list: string[];
+  updated_at: string;
+  website_link: string;
+}
 
 export type data = obj[];
-
-export interface response {
-  data: data;
-}
