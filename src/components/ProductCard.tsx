a51aaaf44f7144975bea502bdd1ce4c9bbec1001
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-27 13:54:38 -0600

    Add brand span

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,15 +1,17 @@
 import styles from "../styles/ProductCard.module.css";
 
 type CardProps = {
+  brand: string;
   name: string;
   price: string;
   imgSrc: string;
 };
 
-const ProductCard = ({ name, price, imgSrc }: CardProps) => {
+const ProductCard = ({ brand, name, price, imgSrc }: CardProps) => {
   return (
     <div className={styles.card}>
       <img src={imgSrc} alt={name} width="400" />
+      <span className={styles.brand}>{brand}</span>
       <span className={styles.name}>{name}</span>
       <span className={styles.price}>{`$${price}`}</span>
     </div>
