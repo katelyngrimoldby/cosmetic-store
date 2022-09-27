c50f01c7c506a6a6efc26182498c712a900977ca
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-27 13:56:20 -0600

    Update ProductCard props

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -49,18 +49,21 @@ const Home = () => {
               slides={[
                 [
                   <ProductCard
+                    brand={data[0].brand}
                     name={data[0].name}
                     price={data[0].price}
                     imgSrc={data[0].api_featured_image}
                     key="0"
                   />,
                   <ProductCard
+                    brand={data[1].brand}
                     name={data[1].name}
                     price={data[1].price}
                     imgSrc={data[1].api_featured_image}
                     key="1"
                   />,
                   <ProductCard
+                    brand={data[2].brand}
                     name={data[2].name}
                     price={data[2].price}
                     imgSrc={data[2].api_featured_image}
@@ -69,18 +72,21 @@ const Home = () => {
                 ],
                 [
                   <ProductCard
+                    brand={data[3].brand}
                     name={data[3].name}
                     price={data[3].price}
                     imgSrc={data[3].api_featured_image}
                     key="3"
                   />,
                   <ProductCard
+                    brand={data[4].brand}
                     name={data[4].name}
                     price={data[4].price}
                     imgSrc={data[4].api_featured_image}
                     key="4"
                   />,
                   <ProductCard
+                    brand={data[5].brand}
                     name={data[5].name}
                     price={data[5].price}
                     imgSrc={data[5].api_featured_image}
@@ -89,18 +95,21 @@ const Home = () => {
                 ],
                 [
                   <ProductCard
+                    brand={data[6].brand}
                     name={data[6].name}
                     price={data[6].price}
                     imgSrc={data[6].api_featured_image}
                     key="6"
                   />,
                   <ProductCard
+                    brand={data[7].brand}
                     name={data[7].name}
                     price={data[7].price}
                     imgSrc={data[7].api_featured_image}
                     key="7"
                   />,
                   <ProductCard
+                    brand={data[8].brand}
                     name={data[8].name}
                     price={data[8].price}
                     imgSrc={data[8].api_featured_image}
@@ -109,18 +118,21 @@ const Home = () => {
                 ],
                 [
                   <ProductCard
+                    brand={data[9].brand}
                     name={data[9].name}
                     price={data[9].price}
                     imgSrc={data[9].api_featured_image}
                     key="9"
                   />,
                   <ProductCard
+                    brand={data[10].brand}
                     name={data[10].name}
                     price={data[10].price}
                     imgSrc={data[10].api_featured_image}
                     key="10"
                   />,
                   <ProductCard
+                    brand={data[11].brand}
                     name={data[11].name}
                     price={data[11].price}
                     imgSrc={data[11].api_featured_image}
