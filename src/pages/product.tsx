a79423ae884d4a95b26bb216fc624a3ab991732f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-09-29 10:09:06 -0600

    Refine rendering logic

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,13 +1,12 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useAppSelector } from "../redux/hooks";
 import { selectData } from "../redux/slices/dataSlice";
 import { data, obj } from "../apiTypes";
 import NoMatch from "./noMatch";
 
-const findItem = (data: data, id: number): undefined | obj => {
-  if (isNaN(id)) {
-    return undefined;
+const findItem = (data: data, id: number): undefined | obj | false => {
+  if (isNaN(id) || !data.find((e) => e.id === id)) {
+    return false;
   } else {
     return data.find((e) => e.id === id);
   }
@@ -17,17 +16,20 @@ const Product = () => {
   const data = useAppSelector(selectData);
   const params = useParams();
 
-  const item = params.id ? findItem(data, parseInt(params.id)) : undefined;
+  const item: undefined | obj | false = params.id
+    ? findItem(data, parseInt(params.id))
+    : undefined;
 
   return (
     <>
-      {item ? (
-        <div>
-          <h1>{item.name}</h1>
-        </div>
-      ) : (
-        <NoMatch />
-      )}
+      {data.length > 0 &&
+        (item == false ? (
+          <NoMatch />
+        ) : item ? (
+          <div>
+            <h1>{item.name}</h1>
+          </div>
+        ) : null)}
     </>
   );
 };
