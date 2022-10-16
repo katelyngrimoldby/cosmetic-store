5b8ab0239936d9e4de7905c52e2f308a33c0b474
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-10-15 20:20:34 -0600

    Apply styles for filter & sort

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -9,6 +9,8 @@ import styles from "../styles/shop.module.css";
 const Shop = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageLength, setPageLength] = useState(20);
+  const [filter, setFilter] = useState(false);
+  const [sort, setSort] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const data = useAppSelector(selectData);
@@ -89,11 +91,14 @@ const Shop = () => {
         setSearchParams(searchParams);
       }
     }
+
+    setFilter(false);
   };
 
   const handleSort = (event: React.ChangeEvent<HTMLInputElement>) => {
     searchParams.set("sort", event.target.id);
     setSearchParams(searchParams);
+    setSort(false);
   };
 
   const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -106,212 +111,215 @@ const Shop = () => {
         <h1>Shop</h1>
       </header>
       <div className={styles.body}>
-        <div>
-          <ul>
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="none"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === null}
-              />
-              <label htmlFor="blush">None</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="blush"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "blush"}
-              />
-              <label htmlFor="blush">Blush</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="bronzer"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "bronzer"}
-              />
-              <label htmlFor="bronzer">Bronzer</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="eyebrow"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "eyebrow"}
-              />
-              <label htmlFor="eyebrow">Brows</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="eyeliner"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "eyeliner"}
-              />
-              <label htmlFor="eyeliner">Eyeliner</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="eyeshadow"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "eyeshadow"}
-              />
-              <label htmlFor="eyeshadow">Eyeshadow</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="foundation"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "foundation"}
-              />
-              <label htmlFor="foundation">Foundation</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="lip_liner"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "lip_liner"}
-              />
-              <label htmlFor="lip_liner">Lip Liner</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="lipstick"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "lipstick"}
-              />
-              <label htmlFor="lipstick">Lipstick</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="mascara"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "mascara"}
-              />
-              <label htmlFor="mascara">Mascara</label>
-            </li>
-
-            <li>
-              <input
-                type="radio"
-                name="filter"
-                id="nail_polish"
-                onChange={handleFilter}
-                checked={searchParams.get("filter") === "nail_polish"}
-              />
-              <label htmlFor="nail_polish">Nails</label>
-            </li>
-          </ul>
-          <ul>
-            <li>
-              <input
-                type="radio"
-                name="sort"
-                id="default"
-                onChange={handleSort}
-                checked={searchParams.get("sort") === "default"}
-              />
-              <label htmlFor="default">Default</label>
-            </li>
-            <li>
-              <input
-                type="radio"
-                name="sort"
-                id="name_asc"
-                onChange={handleSort}
-                checked={searchParams.get("sort") === "name_asc"}
-              />
-              <label htmlFor="name_asc">Name (asc.)</label>
-            </li>
-            <li>
-              <input
-                type="radio"
-                name="sort"
-                id="name_desc"
-                onChange={handleSort}
-                checked={searchParams.get("sort") === "name_desc"}
-              />
-              <label htmlFor="name_desc">Name (desc.)</label>
-            </li>
-            <li>
-              <input
-                type="radio"
-                name="sort"
-                id="price_asc"
-                onChange={handleSort}
-                checked={searchParams.get("sort") === "price_asc"}
-              />
-              <label htmlFor="price_asc">Price (asc.)</label>
-            </li>
-            <li>
-              <input
-                type="radio"
-                name="sort"
-                id="price_desc"
-                onChange={handleSort}
-                checked={searchParams.get("sort") === "price_desc"}
-              />
-              <label htmlFor="price_desc">Price (desc.)</label>
-            </li>
-          </ul>
+        <div className={styles.settings}>
+        <div className={styles.setting}>
+          <span>Filter</span>
+          <button onClick={()=>{
+            setFilter(!filter);
+          }}>Filter {filter ? '^' : 'v'}</button>
+            <ul className={filter ? styles.visible : styles.invisible}>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="none"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === null}
+                />
+                <label htmlFor="blush">None</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="blush"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "blush"}
+                />
+                <label htmlFor="blush">Blush</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="bronzer"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "bronzer"}
+                />
+                <label htmlFor="bronzer">Bronzer</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="eyebrow"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "eyebrow"}
+                />
+                <label htmlFor="eyebrow">Brows</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="eyeliner"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "eyeliner"}
+                />
+                <label htmlFor="eyeliner">Eyeliner</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="eyeshadow"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "eyeshadow"}
+                />
+                <label htmlFor="eyeshadow">Eyeshadow</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="foundation"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "foundation"}
+                />
+                <label htmlFor="foundation">Foundation</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="lip_liner"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "lip_liner"}
+                />
+                <label htmlFor="lip_liner">Lip Liner</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="lipstick"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "lipstick"}
+                />
+                <label htmlFor="lipstick">Lipstick</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="mascara"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "mascara"}
+                />
+                <label htmlFor="mascara">Mascara</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="filter"
+                  id="nail_polish"
+                  onChange={handleFilter}
+                  checked={searchParams.get("filter") === "nail_polish"}
+                />
+                <label htmlFor="nail_polish">Nails</label>
+              </li>
+            </ul>
         </div>
-        <div className={styles.grid}>
-          {currentData.map((e, i) => {
-            return (
-              <ProductCard
-                id={e.id}
-                brand={typeof e.brand != "string" ? "filler" : e.brand}
-                name={e.name}
-                price={typeof e.price != "string" ? "5.0" : e.price}
-                imgSrc={e.api_featured_image}
-                key={i}
-              />
-            );
-          })}
+        <div className={styles.setting}>
+          <span>Sort</span>
+          <button onClick={()=>{
+            setSort(!sort);
+          }}>Sort {sort ? '^' : 'v'}</button>
+            <ul className={sort ? styles.visible : styles.invisible} id="sort">
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="default"
+                  onChange={handleSort}
+                  checked={searchParams.get("sort") === "default"}
+                />
+                <label htmlFor="default">Default</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="name_asc"
+                  onChange={handleSort}
+                  checked={searchParams.get("sort") === "name_asc"}
+                />
+                <label htmlFor="name_asc">Name (asc.)</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="name_desc"
+                  onChange={handleSort}
+                  checked={searchParams.get("sort") === "name_desc"}
+                />
+                <label htmlFor="name_desc">Name (desc.)</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="price_asc"
+                  onChange={handleSort}
+                  checked={searchParams.get("sort") === "price_asc"}
+                />
+                <label htmlFor="price_asc">Price (asc.)</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  name="sort"
+                  id="price_desc"
+                  onChange={handleSort}
+                  checked={searchParams.get("sort") === "price_desc"}
+                />
+                <label htmlFor="price_desc">Price (desc.)</label>
+              </li>
+            </ul>
         </div>
-        <Pagination
-          currentPage={currentPage}
-          totalCount={dataCopy.length}
-          pageSize={pageLength}
-          onPageChange={(page) => setCurrentPage(page)}
-        />
-
-        <span>
-          Display{" "}
-          <select name="items" id="items" onChange={handleSelect}>
-            <option value="20">20</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>{" "}
-          items per page
-        </span>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.grid}>
+            {currentData.map((e, i) => {
+              return (
+                <ProductCard
+                  id={e.id}
+                  brand={typeof e.brand != "string" ? "filler" : e.brand}
+                  name={e.name}
+                  price={typeof e.price != "string" ? "5.0" : e.price}
+                  imgSrc={e.api_featured_image}
+                  key={i}
+                />
+              );
+            })}
+          </div>
+          <Pagination
+            currentPage={currentPage}
+            totalCount={dataCopy.length}
+            pageSize={pageLength}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+          <span>
+            Display{" "}
+            <select name="items" id="items" onChange={handleSelect}>
+              <option value="20">20</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>{" "}
+            items per page
+          </span>
+        </div>
       </div>
     </main>
   );
