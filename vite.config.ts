721c2573bcd80e2fe349fb2edb689b1916d3a8d6
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-10-19 13:52:55 -0600

    Add autoprefizer as plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cosmetic-store/',
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer,
+      ]
+    }
+  }
 })
