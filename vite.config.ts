cef2160ba665f42179a605788a53c5896ec9cfa2
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-10-19 14:29:05 -0600

    Remove breaking plugin use

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import autoprefixer from 'autoprefixer'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/cosmetic-store/',
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer,
-      ]
-    }
-  }
+  
 })
